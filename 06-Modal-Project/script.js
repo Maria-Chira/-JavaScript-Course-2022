639c0bb0b4b44572d047b17249f466bc0d87f035
Maria Chira <chiramariageorgiana@gmail.com>
2022-01-05 23:42:40 +0000

    DOOM - Project 2 - Modal Window - Working with Classes - Handling Key Events

---
+++ 06-Modal-Project/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+const btnsOpenModel = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModel);
+
+const openModel = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModel.length; i++) {
+  btnsOpenModel[i].addEventListener('click', openModel);
+}
+
+btnCloseModel.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
